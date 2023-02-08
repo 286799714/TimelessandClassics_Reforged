ac42b3b3de5d3ab3eaedb0614a4131528fabff10
Taoism DeepLake <pymess@126.com>
2023-02-09 00:05:30 +0800

    Most guns causes offhand item ban
    Any offhand non-tac-gun item will be thrown away if main hand is carrying a tac-gun.
    m1911，sti2011，mac10&cz75 are exceptions as they are one-handed gun.

--- src/main/java/com/tac/guns/item/GunItem.java
+++ src/main/java/com/tac/guns/item/GunItem.java
@@ -3,16 +3,21 @@ package com.tac.guns.item;
 import com.tac.guns.common.Gun;
 import com.tac.guns.common.NetworkGunManager;
 import com.tac.guns.enchantment.EnchantmentTypes;
+import com.tac.guns.init.ModItems;
 import com.tac.guns.util.GunEnchantmentHelper;
 import com.tac.guns.util.GunModifierHelper;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.UseAction;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.KeybindTextComponent;
@@ -162,6 +167,32 @@ public class GunItem extends Item implements IColored
             });
         }
         return this.gun;
+    }
+
+    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+        if (isSelected)
+        {
+            if (entityIn instanceof PlayerEntity)
+            {
+                PlayerEntity playerEntity = (PlayerEntity) entityIn;
+                if (!isSingleHanded(stack))
+                {
+                    ItemStack offHand = playerEntity.getHeldItemOffhand();
+                    if (!(offHand.getItem() instanceof GunItem) && !offHand.isEmpty()) {
+                        playerEntity.dropItem(offHand, false);
+                        playerEntity.setHeldItem(Hand.OFF_HAND, ItemStack.EMPTY);
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean isSingleHanded(ItemStack stack)
+    {
+        Item item = stack.getItem();
+        return item == ModItems.M1911.get() || item == ModItems.MICRO_UZI.get()
+                || item == ModItems.STI2011.get() || item == ModItems.CZ75.get();
     }
 
     /*@Override
