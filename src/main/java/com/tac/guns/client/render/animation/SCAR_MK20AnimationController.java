1b8d716ea9c76dce008810aa3ee24aaf39651282
Clumsy Alien <85691268+ClumsyAlien@users.noreply.github.com>
2023-05-08 02:42:28 -0400

    fixed scar mk20 anims

--- src/main/java/com/tac/guns/client/render/animation/SCAR_MK20AnimationController.java
+++ src/main/java/com/tac/guns/client/render/animation/SCAR_MK20AnimationController.java
@@ -26,7 +26,7 @@ public class SCAR_MK20AnimationController extends GunAnimationController{
     public static final AnimationMeta RELOAD_EMPTY = new AnimationMeta(new ResourceLocation("tac","animations/scar_h_reload_empty.gltf"));
     public static final AnimationMeta DRAW = new AnimationMeta(new ResourceLocation("tac","animations/scar_h_draw.gltf"));
     public static final AnimationMeta INSPECT = new AnimationMeta(new ResourceLocation("tac","animations/scar_h_inspect.gltf"));
-    public static final AnimationMeta INSPECT_EMPTY = new AnimationMeta(new ResourceLocation("tac","animations/scar_h_inspect_empty.gltf"));
+    public static final AnimationMeta INSPECT_EMPTY = new AnimationMeta(new ResourceLocation("tac","animations/scar_h_inspect.gltf"));
     public static final AnimationMeta STATIC = new AnimationMeta(new ResourceLocation("tac","animations/scar_h_static.gltf"));
     private static final SCAR_MK20AnimationController instance = new SCAR_MK20AnimationController();
 
