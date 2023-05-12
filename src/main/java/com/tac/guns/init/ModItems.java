fd59f12bb0728e53a22274a8ac9be057b7c0923e
Clumsy Alien <85691268+ClumsyAlien@users.noreply.github.com>
2023-05-12 00:23:54 -0400

    Update ModItems.java

--- src/main/java/com/tac/guns/init/ModItems.java
+++ src/main/java/com/tac/guns/init/ModItems.java
@@ -100,7 +100,7 @@ public class ModItems
     public static final RegistryObject<Item> MK18_MOD1 = REGISTER.register("mk18_mod1", () -> new TimelessGunItem(properties -> properties.group(GunMod.RIFLE), GunModifiers.MK18_MOD1_MOD));
     public static final RegistryObject<Item> UDP_9 = REGISTER.register("udp_9", () -> new TimelessGunItem(properties -> properties.group(GunMod.SMG), GunModifiers.UDP_9_MOD));
     public static final RegistryObject<Item> SCAR_MK20 = REGISTER.register("scar_mk20", () -> new TimelessGunItem(properties -> properties.group(GunMod.SNIPER), GunModifiers.SCAR_H_MOD));
-    public static final RegistryObject<Item> DEVGUN1 = REGISTER.register("devgun1", () -> new TimelessGunItem(properties -> properties.group(GunMod.GROUP), GunModifiers.UDP_9_MOD));
+    //public static final RegistryObject<Item> DEVGUN1 = REGISTER.register("devgun1", () -> new TimelessGunItem(properties -> properties.group(GunMod.GROUP), GunModifiers.UDP_9_MOD));
 
 
     /* Ammunition */
