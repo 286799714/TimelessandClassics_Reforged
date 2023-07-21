ba1c729c5643ab0811293aa28bc56c108342356b
286799714 <41233300+286799714@users.noreply.github.com>
2023-07-22 02:07:16 +0800

    fix RPG weapon type

--- src/main/java/com/tac/guns/common/WeaponType.java
+++ src/main/java/com/tac/guns/common/WeaponType.java
@@ -1,11 +1,11 @@
 package com.tac.guns.common;
 
 public enum WeaponType {
-    AR,
-    MG,
-    PT,
-    SG,
-    SMG,
-    SR,
-    RPG;
+    AR, //0
+    MG, //1
+    PT, //2
+    SG, //3
+    SMG, //4
+    SR, //5
+    RPG; //6
 }
