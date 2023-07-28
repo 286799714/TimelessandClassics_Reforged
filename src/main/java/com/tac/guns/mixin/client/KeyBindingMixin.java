208e232cdd1c88b8f004bce60456c3f1f3479e41
Arcomit <8089341@qq.com>
2023-07-28 12:10:17 +0800

    KeyMixin改用@Inject。 The Key Mixin uses @Inject instead.

--- src/main/java/com/tac/guns/mixin/client/KeyBindingMixin.java
+++ src/main/java/com/tac/guns/mixin/client/KeyBindingMixin.java
@@ -8,6 +8,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -26,8 +29,10 @@ public class KeyBindingMixin{
     @Shadow
     private static KeyBindingMap HASH;
 
-    @Overwrite
-    public static void onTick(InputMappings.Input key) {
+    @Inject(method = "onTick",at = @At("HEAD"),cancellable = true)
+    private static void onTick(InputMappings.Input key, CallbackInfo callbackInfo) {
+        callbackInfo.cancel();
+
         HashSet<TacKeyBingding> pressedTacKeyBindings = new HashSet<>();
         boolean isTriggerOtherKey = true;
 
